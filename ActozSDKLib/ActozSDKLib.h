c6b104768654f9f78407e64a4ae658e3ed0a60cd
구정현 <howhansume@gujeonghyeon-ui-Mac-mini.local>
2013-04-21 10:38:20 +0900

    Initial Commit

---
+++ ActozSDKLib/ActozSDKLib.h
@@ -0,0 +1,13 @@
+//
+//  ActozSDKLib.h
+//  ActozSDKLib
+//
+//  Created by 구정현 on 13. 4. 21..
+//  Copyright (c) 2013년 구정현. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ActozSDKLib : NSObject
+
+@end
---
+++ ActozSDKLib/ActozSDKLib.m
@@ -0,0 +1,13 @@
+//
+//  ActozSDKLib.m
+//  ActozSDKLib
+//
+//  Created by 구정현 on 13. 4. 21..
+//  Copyright (c) 2013년 구정현. All rights reserved.
+//
+
+#import "ActozSDKLib.h"
+
+@implementation ActozSDKLib
+
+@end
