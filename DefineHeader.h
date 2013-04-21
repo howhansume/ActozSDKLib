81a432bfd842a5085e65baf3e859f789972177e9
구정현 <howhansume@gujeonghyeon-ui-Mac-mini.local>
2013-04-21 10:40:18 +0900

    ccccc

---
+++ DefineHeader.h
@@ -0,0 +1,11 @@
+//
+//  DefineHeader.h
+//  ActozSDKLib
+//
+//  Created by 구정현 on 13. 4. 20..
+//  Copyright (c) 2013년 구정현. All rights reserved.
+//
+
+#define WIDTH [UIApplication sharedApplication].keyWindow.bounds.size.width
+#define HEIGHT [UIApplication sharedApplication].keyWindow.bounds.size.height-20
+
