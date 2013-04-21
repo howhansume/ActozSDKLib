d674da28053a2fcf294acd852e025c1b9eceb5f4
howhansume <howhansume@gmail.com>
2013-04-21 12:38:57 +0900

    Update DefineHeader.h

--- DefineHeader.h
+++ DefineHeader.h
@@ -4,7 +4,7 @@
 //
 //  Created by 구정현 on 13. 4. 20..
 //  Copyright (c) 2013년 구정현. All rights reserved.
-//
+//11111
 
 #define WIDTH [UIApplication sharedApplication].keyWindow.bounds.size.width
 #define HEIGHT [UIApplication sharedApplication].keyWindow.bounds.size.height-20
